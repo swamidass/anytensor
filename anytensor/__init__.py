5b8c8ae1fb73d14283d5baefb0be4857ea52afaf
S. Joshua Swamidass <swamidass@gmail.com>
2025-03-02 16:00:00 -0600

    commets

--- anytensor/__init__.py
+++ anytensor/__init__.py
@@ -1,6 +1,6 @@
 from .backends import get_backend
 
-#in port einops functions, which all work the same way as anytensor
+#import einops functions, which all work the same way as anytensor
 from einops import einsum, pack, unpack, rearrange, reduce
 
 from .segment import segment_sum, segment_max, segment_min, segment_mean, segment_normalize
