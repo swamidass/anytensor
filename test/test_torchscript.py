6aa841cbb80c9c60597476dc381783cecbda8df1
S. Joshua Swamidass <swamidass@gmail.com>
2025-03-03 10:00:41 -0600

    testing torchscript jit

---
+++ test/test_torchscript.py
@@ -0,0 +1,31 @@
+import torch as th
+import anytensor as at
+import pytest
+from functools import partial
+
+
+# Torchscript cannot understand the dynamic typing
+@pytest.mark.xfail(reason="Not compatible with torchscript yet")
+def test_jit():
+  @th.jit.script
+  def f(x, s):
+    return at.segment_sum(x, s, 5, backend=at.backends.TorchBackend())
+
+
+# But tracing works just fine.
+def test_trace():
+  
+  x = th.randn(10)
+  s = th.randint(0, 5, (10,))
+
+  @partial(th.jit.trace, example_inputs=(x, s))
+  def f(x, s):
+    return at.segment_sum(x, s, 5)
+
+  # traced function works
+  y = f(x, s)
+
+  # traced function works with different sized inputs
+  x = th.randn(15)
+  s = th.randint(0, 5, (15,))
+  y = f(x, s)
